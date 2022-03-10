24c9e9c82fa26c589df363e02ee1fc0a9a6e2afe
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-03-11 00:54:23 +0100

    Almost finished second task in laboratory no. 1
    Now only need to format attributes.

--- PT_Lab1/Program.cs
+++ PT_Lab1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using ExtensionMethods;
 
 namespace PT_Lab1
 {
@@ -8,16 +9,10 @@ namespace PT_Lab1
         static void Main(string[] args)
         {
             string sourcePath = args[0];
-
-            
-
             string[] allfiles = Directory.GetDirectories(sourcePath);
-
             Console.WriteLine("=================================================================\n");
             Console.WriteLine(sourcePath);
-
             ListContent(sourcePath);
-
             Console.WriteLine("=================================================================\n");
         }
 
@@ -25,26 +20,41 @@ namespace PT_Lab1
         {
             foreach(string directory in Directory.GetDirectories(path,"*",SearchOption.TopDirectoryOnly))
             {
-                Console.WriteLine(Indentation(directory) + "-" +Path.GetFileName(directory));
+                DirectoryInfo dirInfo = new DirectoryInfo(directory);                
+                Console.WriteLine(Indentation(directory) + "-" + Path.GetFileName(directory) + "[" + dirInfo.ContentCount() + "]" );
+                
                 ListContent(directory);
             }
             foreach(string file in Directory.GetFiles(path))
             {
-                Console.WriteLine(Indentation(file) + Path.GetFileName(file));
-                //ListContent(file);
+                FileInfo fileInfo = new FileInfo(file);
+                Console.WriteLine(Indentation(file) + Path.GetFileName(file) + " | " + fileInfo.FormattedAttributes());
             }
         }
 
         static string Indentation(string path)
         {
             string prefix = "";
-
             for(int i = 0; i < Path.GetFullPath(path).Split("\\").Length -2; i++)
-            {
                 prefix += "    ";
-            }
-
             return prefix;
         }
+        
     }
+
 }
+namespace ExtensionMethods
+    {
+        public static class CustomExtenstions
+        {
+            public static string FormattedAttributes(this FileSystemInfo info)
+            {
+                return info.Attributes.ToString();
+            }
+
+            public static string ContentCount(this DirectoryInfo info)
+            {
+                return info.GetFiles().Length.ToString();
+            }
+        }
+    }
